cbf69ac8fdd6443d85e16bd5fd75f9a712fe7651
Евгений Лейченко <gheniavip@gmail.com>
2023-04-29 01:01:00 +0300

    second task

--- task2.php
+++ task2.php
@@ -1,34 +1,20 @@
 <?php
-function birthdayCountdown($date) {
+function birthdayCountdown($date)
+{
     $timestamp = strtotime($date);
-
     $today = time();
-    $days_left = ceil(($timestamp - $today) / 86400);
-    $days_gone = -1*$days_left;
-
-    if ($days_left > 0) {
-        echo "There are $days_left days left until your birthday.";
-    } elseif ($days_left == 0) {
+    $daysLeft = ceil(($timestamp - $today) / 86400);
+    $daysGone = -1 * $daysLeft;
+    if ($daysLeft > 0) {
+        echo "There are $daysLeft days left until your birthday.";
+    } elseif ($daysLeft == 0) {
         echo "Happy birthday!";
     } else {
-        echo "Your birthday was $days_gone days ago.";
+        echo "Your birthday was $daysGone days ago.";
     }
 }
-
-function testTask($number) {
-    switch($number) {
-        case 1:
-            echo birthdayCountdown("24-06-2023");
-            break;
-        case 2:
-            echo birthdayCountdown(date("d-m-Y",time()));
-            break;
-        case 3:
-            echo birthdayCountdown("24-01-2023");
-            break;
-    }
-}
-
-$a = readline("Enter a number from 1-3: ");
-testTask($a);
-?>
+echo birthdayCountdown("24-06-2023");
+echo "\n";
+echo birthdayCountdown(date("d-m-Y", time()));
+echo "\n";
+echo birthdayCountdown("24-01-2023");
