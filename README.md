9dd5cf9ca7769e2dee2941774e46ed6e2a5b11a1
Евгений Лейченко <gheniavip@gmail.com>
2023-04-29 01:18:52 +0300

    task sixth

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # TASK 6
-## Вот некоторые методы для работы со строками :
-+ ucfirst() - первый символ в строке делает в uppercase 
-+ ucwords() - первый символ каждого слова в строке делает в uppercase
-+ preg_split() - делит строку согласно заданному регулярному выражению
+
+## Here are some methods for working with strings :
+
+- ucfirst() - the first character in the string is converted to uppercase
+- ucwords() - the first character of each word in the string is converted to uppercase
+- preg_split() - divides the string according to a given regular expression
--- task6.php
+++ task6.php
@@ -1,28 +1,15 @@
 <?php
-
-function concatCapitalize($str) {
-    $str_split = preg_split("/[_\- ]+/", $str);
-    $str_filtered = array_filter($str_split);
+function concatCapitalize($str)
+{
+    $strSplit = preg_split("/[_\- ]+/", $str);
+    $strFiltered = array_filter($strSplit);
     $capWords = array();
-    foreach ($str_filtered as $word) {
+    foreach ($strFiltered as $word) {
         $capWord = ucfirst(strtolower($word));
         $capWords[] = $capWord;
     }
     return implode("", $capWords);
 }
-
-function testTask($number) {
-    switch($number) {
-        case 1:
-            echo concatCapitalize("              The quick-brown_fox jumps over the_lazy-dog       ");
-            break;
-        case 2:
-            echo concatCapitalize("H- -- ---__ -_- --ello foo_ b___-A r");
-            break;
-    }
-}
-
-$a = readline("Enter a number from 1-2: ");
-testTask($a);
-
-?>
+echo concatCapitalize("              The quick-brown_fox jumps over the_lazy-dog       ");
+echo "\n";
+echo concatCapitalize("H- -- ---__ -_- --ello foo_ b___-A r");
